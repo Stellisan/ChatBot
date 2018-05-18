55f915dd27dbe124f281e01d1f746f7bf90c3439
SA050990 <Sanjay.Arivazhagan@cerner.com>
2018-05-18 15:37:11 +0530

    Correction for Deployment

--- Index.js
+++ Index.js
@@ -1,3 +1,3 @@
-exports.helloHttp = function helloHttp (request, response) {
-    response.json({ fulfillmentText: 'This is a sample response from your webhook!' });
-  };
+module.exports.helloHttp = function helloHttp (request, response) {
+  response.json({ fulfillmentText: 'This is a sample response from your webhook!' });
+};
