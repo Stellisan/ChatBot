b775d69aed9beba2d038512ee3ad563b0a003a96
Stellisan <sanjayarivazhagan3@gmail.com>
2018-05-20 11:01:53 +0530

    changing for deploymernt

--- Index.js
+++ Index.js
@@ -12,10 +12,10 @@ http.createServer(function (request, response) {
    // Send the response body as "Hello World"
    //response.json({ fulfillmentText: 'This is a sample response from your webhook!' });
    response.end('Hello World\n');
-}).listen(8081);
+}).listen(8080);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+//console.log('Server running at http://127.0.0.1:8081/');
 
 module.exports.helloHttp = function helloHttp (request, response) {
   response.json({ fulfillmentText: 'This is a sample response from your webhook!' });
